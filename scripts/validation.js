465345ee1fa305c60eb1de4f21180e67df07454d
Catelyn Richard <catelyn.richard@yahoo.com>
2023-06-22 02:25:34 -0400

    forms validation

---
+++ scripts/validation.js
@@ -0,0 +1,93 @@
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorClass = configObj.errorClass;
+  const inputErrorClass = configObj.inputErrorClass;
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.add(inputErrorClass);
+  errorElement.textContent = inputElement.validationMessage;
+  errorElement.classList.add(errorClass);
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorClass = configObj.errorClass;
+  const inputErrorClass = configObj.inputErrorClass;
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.remove(inputErrorClass);
+  errorElement.textContent = "";
+  errorElement.classList.remove(errorClass);
+};
+
+function isValid(formElement, inputElement) {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+}
+
+function hasInvalidInput(inputList) {
+  return !inputList.every((inputElement) => inputElement.validity.valid);
+}
+
+function disableButton(formElement, submitButton, configObj) {
+  const inactiveButtonClass = configObj.inactiveButtonClass;
+  submitButton.classList.add(inactiveButtonClass);
+  submitButton.disabled = true;
+  return;
+}
+
+function enableButton(formElement, submitButton, configObj) {
+  const inactiveButtonClass = configObj.inactiveButtonClass;
+  submitButton.classList.remove(inactiveButtonClass);
+  submitButton.disabled = false;
+  return;
+}
+
+function toggleButtonState(inputList, submitButton, configObj) {
+  const inactiveButtonClass = configObj.inactiveButtonClass;
+
+  if (hasInvalidInput(inputList)) {
+    submitButton.classList.add(inactiveButtonClass);
+    submitButton.disabled = true;
+  } else {
+    submitButton.classList.remove(inactiveButtonClass);
+    submitButton.disabled = false;
+  }
+}
+
+function setEventListeners(formElement, configObj) {
+  const inputSelector = configObj.inputSelector;
+  const inputList = Array.from(formElement.querySelectorAll(inputSelector));
+  const submitButtonSelector = configObj.submitButtonSelector;
+  const submitButton = formElement.querySelector(submitButtonSelector);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", (evt) => {
+      isValid(formElement, inputElement, configObj);
+      toggleButtonState(inputList, submitButton, configObj);
+    });
+  });
+}
+
+function enableValidation(configObj) {
+  const formList = Array.from(
+    document.querySelectorAll(configObj.formSelector)
+  );
+  formList.forEach((formElement) => {
+    formElement.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+
+    setEventListeners(formElement, configObj);
+  });
+}
+
+const configObj = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__save-button",
+  inactiveButtonClass: "modal__save-button_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__error_visible",
+};
+
+enableValidation(configObj);
